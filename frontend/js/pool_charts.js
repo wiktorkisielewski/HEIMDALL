ba0f9e5a16650b2f07efc0fe87badb41f7170aa7
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2021-05-14 09:14:51 +0200

    0.1.3

--- frontend/js/pool_charts.js
+++ frontend/js/pool_charts.js
@@ -1,37 +1,44 @@
-var pool_depth = {
-  labels: ["1", "2", "3", "4", "6", "7"],
+var chart_data = {
+  labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   datasets: [{
     label: "Pool Depth",
-    backgroundColor: "rgba(107,225,208,0.2)",
-    borderColor: "rgba(107,225,208,0.2)",
+    backgroundColor: "#54FFD820",
+    borderColor: "#54FFD820",
     borderWidth: 2,
-    hoverBackgroundColor: "rgba(107,225,208,0.2)",
-    hoverBorderColor: "rgba(107,225,208,0.2)",
-    data: [65, 59, 20, 81, 56, 55, 40],
-  }]
+    hoverBackgroundColor: "#54FFD820",
+    hoverBorderColor: "#54FFD820",
+    data: [49, 47, 51, 48, 45, 44, 40, 42, 47, 45],
+  },
+  {
+    label: "Price",
+    backgroundColor: "#54FFD840",
+    borderColor: "#54FFD840",
+    borderWidth: 2,
+    hoverBackgroundColor: "#54FFD840",
+    hoverBorderColor: "#54FFD840",
+    data: [11, 9, 7, 10, 9, 12, 16, 15, 18, 22],
+  },
+  {
+    label: "Rune Depth",
+    backgroundColor: "#54FFD860",
+    borderColor: "#54FFD860",
+    borderWidth: 2,
+    hoverBackgroundColor: "#54FFD860",
+    hoverBorderColor: "#54FFD860",
+    data: [32, 35, 33, 31, 29, 34, 35, 37, 36, 34],
+  }
+  ]
 };
 
-var price = {
-  labels: ["1", "2", "3", "4", "6", "7"],
-  datasets: [{
-    label: "Price",
-    backgroundColor: "rgba(107,225,208,0.2)",
-    borderColor: "rgba(107,225,208,0.2)",
-    borderWidth: 2,
-    hoverBackgroundColor: "rgba(107,225,208,0.2)",
-    hoverBorderColor: "rgba(107,225,208,0.2)",
-    data: [21, 33, 37, 42, 27, 33, 36],
-  }]
-};
 
 var options = {
   maintainAspectRatio: false,
   scales: {
     yAxes: [{
-      stacked: true,
+      stacked: false,
       gridLines: {
         display: true,
-        color: "rgba(107,225,208,0.2)"
+        color: "#54FFD855"
       }
     }],
     xAxes: [{
@@ -39,12 +46,16 @@ var options = {
         display: false
       }
     }]
-  }
+  },
+  legend: {
+      display: true,
+      position: "bottom"
+    },
 };
 
 Chart.Line('bnb.bnb_test_chart', {
   options: options,
-  data: pool_depth
+  data: chart_data
 });
 
 
